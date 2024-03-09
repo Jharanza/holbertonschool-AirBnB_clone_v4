6dec3879d1e894ff7e8ef6b135ed453f09be307b
jhony arana <jharanza1986@gmail.com>
2024-03-09 14:42:24 -0500

    feat: Select the amenities dinamically

--- README.md
+++ README.md
@@ -158,6 +158,7 @@ Alexa Orrico - [Github](https://github.com/alexaorrico) / [Twitter](https://twit
 Jennifer Huang - [Github](https://github.com/jhuang10123) / [Twitter](https://twitter.com/earthtojhuang)  
 Jhoan Zamora - [Github](https://github.com/jzamora5) / [Twitter](https://twitter.com/JhoanZamora10)  
 David Ovalle - [Github](https://github.com/Nukemenonai) / [Twitter](https://twitter.com/disartDave)
+Jhony Arana - [Github](https://github.com/jharanza) / [Linkedin](linkedin.com/in/jhony-arana-carranza-a103b350)
 
 Second part of Airbnb: Joann Vuong
 ## License
---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,22 @@
+$(document).ready(() => {
+  const amenityId = {};
+
+  const checkbox = $('.amenities input[type="checkbox"]');
+  checkbox.prop('checked', false);
+
+  checkbox.change(function() { // Use function() instead of arrow function to preserve 'this' context
+    const dataId = $(this).attr('data-id');
+    const dataName = $(this).attr('data-name');
+
+    if (this.checked) {
+      amenityId[dataId] = dataName;
+    } else {
+      // delete amenityId[dataId];
+    }
+
+    const output = Object.values(amenityId).join(', ') || '&nbsp;'; // Use Object.values to extract values
+    $('div.amenities > h4').html(output); // Use html() to insert HTML content
+
+    checkbox.trigger('change');
+  });
+});
