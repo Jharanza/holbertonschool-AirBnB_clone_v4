19f6792bf7cf77f7285f9d85c25d694a17676169
jhony arana <jharanza1986@gmail.com>
2024-03-10 22:43:45 -0500

    feat: active the button

---
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -0,0 +1,105 @@
+$(document).ready(() => {
+  const amenityId = {};
+
+  const checkbox = $('.amenities input[type="checkbox"]');
+  checkbox.prop('checked', false);
+
+  checkbox.change(function() { 
+    const dataId = $(this).attr('data-id');
+    const dataName = $(this).attr('data-name');
+
+    if (this.checked) {
+      amenityId[dataId] = dataName;
+    } else {
+      delete amenityId[dataId];
+    }
+
+    const output = Object.values(amenityId).join(', ') || '&nbsp;'; 
+    $('div.amenities > h4').html(output);
+  });
+
+  $.ajax({
+    type: "GET",
+    url: "http://127.0.0.1:5001/api/v1/status/",
+    dataType: "json"
+  }).done((data) => {
+    if (data.status === 'OK')
+      $("div#api_status").addClass('available');
+    else
+      $("div#api_status").removeClass('available');
+  });
+
+  $.ajax({
+    method: "POST",
+    url: "http://127.0.0.1:5001/api/v1/places_search/",
+    data: JSON.stringify({}),
+    contentType: 'application/json',
+    dataType: 'json',
+  }).done( (response) => {
+    let output = ``;
+    response.forEach(place => {
+      output += `
+      <article>
+        <div class="title_box">
+          <h2>${ place.name }</h2>
+          <div class="price_by_night">$${ place.price_by_night }</div>
+        </div>
+        <div class="information">
+          <div class="max_guest">${ place.max_guest } Guests</div>
+            <div class="number_rooms">${ place.number_rooms } Bedrooms</div>
+            <div class="number_bathrooms">${ place.number_bathrooms } Bathrooms</div>
+        </div>
+        <div class="description">${ place.description }</div>
+      </article>
+      `
+    });
+    $('.places').append(output);
+  }).fail((error) => {
+    console.log("Error " + error);
+  });
+
+  $('button').on('click', () => {
+    $.ajax({
+      method: 'POST',
+      url: 'http://127.0.0.1:5001/api/v1/places_search/',
+      data: JSON.stringify({ 'amenities': Object.keys(amenityId) }),
+      contentType: 'application/json',
+      dataType: 'json'
+    }).done((response) => {
+      $('.places').html('<br />');
+      response.forEach(place => {
+        let output = '';
+        $.ajax({
+          method: 'GET',
+          url: `http://127.0.0.1:5001/api/v1/places/${place.id}/amenities`
+        }).done(data => {
+          let amenitiesID = [];
+          data.forEach(aMeNity => {
+            amenitiesID.push(aMeNity.name);
+          });
+          const amenity = amenitiesID.join(', ');
+          output += `
+          <article>
+            <div class="title_box">
+              <h2>${ place.name }</h2>
+              <div class="price_by_night">$${ place.price_by_night }</div>
+            </div>
+            <div class="information">
+              <div class="max_guest">${ place.max_guest } Guest</div>
+              <div class="number_rooms">${ place.number_rooms } Bedroom</div>
+              <div class="number_bathrooms">${ place.number_bathrooms } Bathroom</div>
+            </div>
+            <div class="user">
+              <b>Amenities:</b> ${ amenity }
+            </div>
+            <div class="description">${ place.description }</div>
+          </article>
+          `;
+          $('.places').append(output);
+        })
+      })
+    }).fail(err => {
+      console.log('Error ' + err);
+    })
+  })
+});
